84dc579240d8d27068065d81e755016e5a8f17ea
Andy Mardell <andy@sidigital.co>
2020-04-10 14:42:06 +0100

    Remove NewsAPI

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   env: {
-    NEWS_API: 'https://newsapi.org/v2',
-    NEWS_API_KEY: process.env.NEWS_API_KEY,
     BING_API: 'https://teletext.cognitiveservices.azure.com/bing/v7.0',
     BING_API_KEY: process.env.BING_API_KEY
   }
