06e95194c8dd673f90402f2be12e50b88943eed2
Andy Mardell <andy@sidigital.co>
2023-07-09 02:21:03 +0100

    Update environment for Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  env: {
-    BING_API: 'https://api.bing.microsoft.com/v7.0',
-    BING_API_KEY: process.env.BING_API_KEY,
-    FOOTBALL_API: 'https://api-football-v1.p.rapidapi.com/v2',
-    FOOTBALL_API_KEY: process.env.FOOTBALL_API_KEY,
-  },
-}
+module.exports = {}
